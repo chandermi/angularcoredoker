6903df9e30a21b3e95b3e870609daf4c00065f5a
Chander <chander.k@live.com>
2020-07-28 13:29:47 +0530

    api

--- WebApp/Controllers/WeatherForecastController.cs
+++ WebApp/Controllers/WeatherForecastController.cs
@@ -49,5 +49,19 @@ namespace WebApp.Controllers
             })
             .ToArray();
         }
+
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get_3()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
