4185e764fc752bd1f163c1ee36f8a921d7bfaf73
Chander <chander.k@live.com>
2020-07-28 17:18:34 +0530

    code changes

--- WebApp/Controllers/WeatherForecastController.cs
+++ WebApp/Controllers/WeatherForecastController.cs
@@ -36,32 +36,5 @@ namespace WebApp.Controllers
             .ToArray();
         }
 
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get_2()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get_3()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
     }
 }
